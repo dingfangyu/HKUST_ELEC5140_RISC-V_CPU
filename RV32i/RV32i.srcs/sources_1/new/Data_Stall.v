10516d3bcbcedde113f214b5282d97ccbe3fe924
丁小明 <ding@dfy.local>
2025-05-18 07:49:41 +0800

    debug

--- RV32i/RV32i.srcs/sources_1/new/Data_Stall.v
+++ RV32i/RV32i.srcs/sources_1/new/Data_Stall.v
@@ -49,7 +49,7 @@ module Data_Stall(
                 IF_ID_dstall = 1;
                 ID_EXE_dstall = 1;
         end
-        else if (EXE_MEM_written_reg != 0 && ((EXE_MEM_written_reg == IF_ID_read_reg1 && ForwardA != 2'b10) || (EXE_MEM_written_reg == IF_ID_read_reg2 && ForwardB != 2'b01))) begin
+        else if (EXE_MEM_written_reg != 0 && ((EXE_MEM_written_reg == IF_ID_read_reg1 && ForwardA != 2'b10) || (EXE_MEM_written_reg == IF_ID_read_reg2 && ForwardB != 2'b10))) begin
                 PC_dstall = 1;
                 IF_ID_dstall = 1;
                 ID_EXE_dstall = 1;
