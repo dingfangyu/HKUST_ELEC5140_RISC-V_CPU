667ef37b615678341f6f23a91348fa13d19fd29a
丁小明 <ding@dfy.local>
2025-05-19 05:17:27 +0800

    debug

--- RV32i/RV32i.srcs/sources_1/new/RV32iPCPU.v
+++ RV32i/RV32i.srcs/sources_1/new/RV32iPCPU.v
@@ -545,7 +545,7 @@ module RV32iPCPU(
     wire [31:0] LoA_data;
 
     // if branch, 00000
-    assign Wt_addr[4:0] = (MEM_WB_inst_in[6:0] != 7'b1100011) MEM_WB_inst_in[11:7] : 5'b00000; // rd, except for branch and store instructions
+    assign Wt_addr[4:0] = (MEM_WB_inst_in[6:0] != 7'b1100011) ? MEM_WB_inst_in[11:7] : 5'b00000; // rd, except for branch and store instructions
     LUI_or_AUIPC _loa_ (
         .inst_in(MEM_WB_inst_in[31:0]),
         .PC(MEM_WB_PC),
