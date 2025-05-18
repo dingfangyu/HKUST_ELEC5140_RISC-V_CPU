53822a73557293871a5d7ed493911c980e6d1092
丁小明 <ding@dfy.local>
2025-05-18 10:32:39 +0800

    debug

--- RV32i/RV32i.srcs/sources_1/new/Data_Stall.v
+++ RV32i/RV32i.srcs/sources_1/new/Data_Stall.v
@@ -47,12 +47,12 @@ module Data_Stall(
         PC_dstall = 0;
         IF_ID_dstall = 0;
         ID_EXE_dstall = 0;
-        if (ID_EXE_written_reg != 0 && ((ID_EXE_written_reg == IF_ID_read_reg1 || ID_EXE_written_reg == IF_ID_read_reg2)) && ID_EXE_DatatoReg != 2'b00) begin 
-                PC_dstall = 1;
-                IF_ID_dstall = 1;
-                ID_EXE_dstall = 1;
+        // if (ID_EXE_written_reg != 0 && ((ID_EXE_written_reg == IF_ID_read_reg1 || ID_EXE_written_reg == IF_ID_read_reg2)) && ID_EXE_DatatoReg != 2'b00) begin 
+        //         PC_dstall = 1;
+        //         IF_ID_dstall = 1;
+        //         ID_EXE_dstall = 1;
 
-        end
+        // end
         
         // if (EXE_MEM_written_reg != 0 && ((EXE_MEM_written_reg == ID_EXE_read_reg1 && ForwardA == 2'b00) || (EXE_MEM_written_reg == ID_EXE_read_reg2 && ForwardB == 2'b00))) begin
         //         PC_dstall = 1;
@@ -65,43 +65,43 @@ module Data_Stall(
         //         ID_EXE_dstall = 1;
         // end
 
-        // if ((ID_EXE_written_reg != 0 && (ID_EXE_written_reg == IF_ID_read_reg1 || ID_EXE_written_reg == IF_ID_read_reg2)) || (EXE_MEM_written_reg != 0 && (EXE_MEM_written_reg == IF_ID_read_reg1 || EXE_MEM_written_reg == IF_ID_read_reg2))) begin
+        if ((ID_EXE_written_reg != 0 && (ID_EXE_written_reg == IF_ID_read_reg1 || ID_EXE_written_reg == IF_ID_read_reg2)) || (EXE_MEM_written_reg != 0 && (EXE_MEM_written_reg == IF_ID_read_reg1 || EXE_MEM_written_reg == IF_ID_read_reg2))) begin
                 
-        // if (
-        //         // r1 is being written by some other insts (E or M), and can be forwarded from (E or M)
-        //         (
-        //                 // E
-        //                 (ID_EXE_written_reg != 0 && ID_EXE_written_reg == IF_ID_read_reg1 && ForwardA == 2'b01)
-        //                 ||  
-        //                 // M    
-        //                 (EXE_MEM_written_reg != 0 && EXE_MEM_written_reg == IF_ID_read_reg1 && ForwardA == 2'b10)
-        //                 ||
-        //                 // no other insts are writting r1
-        //                 (ID_EXE_written_reg != IF_ID_read_reg1 && EXE_MEM_written_reg != IF_ID_read_reg1)
-        //         )  
-        //         && 
-        //         // r2 
-        //         (
-        //                 // E
-        //                 (ID_EXE_written_reg != 0 && ID_EXE_written_reg == IF_ID_read_reg2 && ForwardB == 2'b01)
-        //                 ||  
-        //                 // M    
-        //                 (EXE_MEM_written_reg != 0 && EXE_MEM_written_reg == IF_ID_read_reg2 && ForwardB == 2'b10)
-        //                 ||
-        //                 // no other insts are writting r2
-        //                 (ID_EXE_written_reg != IF_ID_read_reg2 && EXE_MEM_written_reg != IF_ID_read_reg2)
-        //         ) 
-        // ) begin 
-        //         PC_dstall = 0;
-        //         IF_ID_dstall = 0;
-        //         ID_EXE_dstall = 0;
-        // end else begin
-        //         PC_dstall = 1;
-        //         IF_ID_dstall = 1;
-        //         ID_EXE_dstall = 1;
-        // end
+        if (
+                // r1 is being written by some other insts (E or M), and can be forwarded from (E or M)
+                (
+                        // E
+                        (ID_EXE_written_reg != 0 && ID_EXE_written_reg == IF_ID_read_reg1 && ForwardA == 2'b01)
+                        ||  
+                        // M    
+                        (EXE_MEM_written_reg != 0 && EXE_MEM_written_reg == IF_ID_read_reg1 && ForwardA == 2'b10)
+                        ||
+                        // no other insts are writting r1
+                        (ID_EXE_written_reg != IF_ID_read_reg1 && EXE_MEM_written_reg != IF_ID_read_reg1)
+                )  
+                && 
+                // r2 
+                (
+                        // E
+                        (ID_EXE_written_reg != 0 && ID_EXE_written_reg == IF_ID_read_reg2 && ForwardB == 2'b01)
+                        ||  
+                        // M    
+                        (EXE_MEM_written_reg != 0 && EXE_MEM_written_reg == IF_ID_read_reg2 && ForwardB == 2'b10)
+                        ||
+                        // no other insts are writting r2
+                        (ID_EXE_written_reg != IF_ID_read_reg2 && EXE_MEM_written_reg != IF_ID_read_reg2)
+                ) 
+        ) begin 
+                PC_dstall = 0;
+                IF_ID_dstall = 0;
+                ID_EXE_dstall = 0;
+        end else begin
+                PC_dstall = 1;
+                IF_ID_dstall = 1;
+                ID_EXE_dstall = 1;
+        end
 
-        // end
+        end
     end
 
     
