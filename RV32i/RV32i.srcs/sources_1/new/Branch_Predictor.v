82e626d14920aadc8b24c51b3f13eed95862f50d
DING <fdingad@ust.hk>
2025-05-24 22:48:47 +0800

    debug

---
+++ RV32i/RV32i.srcs/sources_1/new/Branch_Predictor.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.05.2025 22:48:16
+// Design Name: 
+// Module Name: Branch_Predictor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Branch_Predictor(
+
+    );
+endmodule
