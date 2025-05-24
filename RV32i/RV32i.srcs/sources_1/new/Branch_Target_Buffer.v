bff913f549596a8024e8b7bfdbac895c6e55dcfd
DING <fdingad@ust.hk>
2025-05-24 21:21:58 +0800

    debug

---
+++ RV32i/RV32i.srcs/sources_1/new/Branch_Target_Buffer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.05.2025 21:21:48
+// Design Name: 
+// Module Name: Branch_Target_Buffer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Branch_Target_Buffer(
+
+    );
+endmodule
