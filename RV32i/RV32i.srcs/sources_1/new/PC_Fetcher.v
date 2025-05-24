f879640955dd7ca34d3094bd2a75557c17ca466a
DING <fdingad@ust.hk>
2025-05-24 23:50:14 +0800

    debug

---
+++ RV32i/RV32i.srcs/sources_1/new/PC_Fetcher.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.05.2025 23:50:03
+// Design Name: 
+// Module Name: PC_Fetcher
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC_Fetcher(
+
+    );
+endmodule
